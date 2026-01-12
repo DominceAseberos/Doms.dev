685a8a51348d1d74d09631128ae2a6e91c1a407c
Domincee <daseberos@gmail.com>
2026-01-12 18:36:24 +0800

    initial layout Mobile view

--- Doms.dev-v2/src/App.jsx
+++ Doms.dev-v2/src/App.jsx
@@ -17,14 +17,19 @@ function App() {
           <div className='bento-card w-48'>
             <p>Profie</p>
             </div>
-            <div className='w-full flex flex-col gap-4'>
+            <div className='w-full flex flex-col gap-2'>
             <p className='bento-card h-full'>Music Player</p>
             <p className='bento-card h-12' >Theme toggle</p>
             </div>
         </div>
-        </div>
+       </div>
+        {/* ABOUT ME */}
+     <div className="
+        bento-card col-span-4 row-span-2   
+        ">About Me</div>
 
-      {/* Interactive section */}        
+        
+      {/* Interactive section */}  
         <div className="
          col-span-4 row-span-3 ">
           <div className='flex justify-betweeen gap-4 h-full w-full'>
@@ -38,31 +43,32 @@ function App() {
         </div>
 
 
-        <div className="
-        bento-card col-span-2 row-span-2   
-        ">Status</div>
-
-        {/* Middle Section */}
-        <div className="
-        bento-card col-span-4 row-span-7 
-       ">Tech Stack</div>
-        <div className="
-        bento-card col-span-4 row-span-5 
-        md:col-span-3 row-span-5 border-blue">
-           <h2>View My Projects</h2>
+       {/* PROJECT */} 
+     
+        <div className="bento-card  border-blue col-span-4 row-span-4 ">
+          Tech Stack
         </div>
+        
+       
         <div className="
-        bento-card col-span-2 row-span-3 ">Stats</div>
+        bento-card col-span-4 row-span-2 ">Stats</div>
 
         {/* Bottom Section */}
-        <div className="bento-card col-span-2 ">
-          <h2>chat bot</h2>
-           {/* GitHub API Activity Feature */}
-        </div>
-        <div className="bento-card col-span-4 row-span-4  border-purple">
-                   <h2>music</h2>
+   
+        <div className="col-span-4 row-span-2 ">
+        <div className='flex justify w-full h-full gap-4'>
+          
+            <div className='w-full flex flex-col gap-4'>
+              <p className='bento-card h-12' >Theme toggle</p>
+              <p className='bento-card h-full'>Music Player</p>
+            </div>
 
+            <div className='bento-card w-48'>
+              <p>Profie</p>
+            </div>
+         
         </div>
+       </div>
 
       </div>
     </main>
