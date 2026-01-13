c09c4667a6f22b44be9d82214d2d664dd59bec3a
Domincee <daseberos@gmail.com>
2026-01-13 19:36:13 +0800

    useState, useEfffect play/pause

--- Doms.dev-v2/src/Componenets/MusicPlayer.jsx
+++ Doms.dev-v2/src/Componenets/MusicPlayer.jsx
@@ -1,7 +1,19 @@
 import '../Style/musicStyle.css'
 
+
+import { useState, useEffect, useCallback } from 'react';
 const MusicPlayer = () => {
    
+  const [isPlaying, setPlaying ] = useState(false);
+
+
+  const togglePlayPause = useCallback (() =>{
+     if(!isPlaying){
+      setPlaying(true)
+     }else
+      setPlaying(false)
+    }
+  )
   return (
     <>
     <div className='music-style flex flex-col gap-2 justify-around'>
@@ -19,11 +31,26 @@ const MusicPlayer = () => {
         </button>
         {/* PLAY */}
 
-        <button>
-          <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+        <button onClick={togglePlayPause}>
+
+          {isPlaying ?(
+             <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 6H8a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h1a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1Zm7 0h-1a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h1a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1Z"/>
+          </svg>
+          
+          )
+          :
+             <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
             <path fill-rule="evenodd" d="M8.6 5.2A1 1 0 0 0 7 6v12a1 1 0 0 0 1.6.8l8-6a1 1 0 0 0 0-1.6l-8-6Z" clip-rule="evenodd" />
           </svg>
+          }
+         
+
+       
         </button>
+
+      
+
         {/* FORWARD */}
         <button>
           <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
