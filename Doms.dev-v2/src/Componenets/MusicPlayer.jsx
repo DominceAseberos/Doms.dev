527eeea852842688e6abcb45e015a0df76178b7b
Domincee <daseberos@gmail.com>
2026-01-14 15:07:10 +0800

    playable music play/pause

--- Doms.dev-v2/src/Componenets/MusicPlayer.jsx
+++ Doms.dev-v2/src/Componenets/MusicPlayer.jsx
@@ -9,16 +9,22 @@ const MusicPlayer = () => {
   const containerRef = useRef(null)
   const [shouldSlide, setShouldSlide] = useState(false);
   const [coverPhotoSrc, setCoverPhotoSrc] = useState("/placeholderAlbum.jpg")
-    const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
+  const [durationSlide, setDurationSlide] = useState(12);
 
+  const audioRef = useRef(new Audio());
 
-  const MAX_COUNT = 5;
-  const FETCH_DELAY = 2000;
 
 /* TOGGLE BUTTONS */
   const togglePlayPause = useCallback(() => {
+    if(isPlaying){
+        audioRef.current.pause()
+    }else{
+      audioRef.current.play().catch(e => console.log("Playback prevented:", e));
+    }
     setPlaying(prev => !prev);
-  }, []);
+  }, [isPlaying]);
+
 
 useEffect(() => {
     const fetchMusicToPlay = async () => {
@@ -28,6 +34,13 @@ useEffect(() => {
         );
         const track = res.data.data;
         setCurrentPlaying(track);
+
+        const streamURL = `https://api.audius.co/v1/tracks/${track.id}/stream?app_name=MyPortfolio`;
+        console.log(streamURL);
+        audioRef.current.src = streamURL;
+        audioRef.current.load();
+        setPlaying(false);
+        
 
         if (track.artwork?.["480x480"]) {
           const img = new Image();
@@ -52,6 +65,11 @@ useEffect(() => {
     };
 
     fetchMusicToPlay();
+
+      return () => {
+      audioRef.current.pause();
+      audioRef.current.src = "";
+    };
   }, []);
 
   
@@ -59,15 +77,29 @@ const titleArtist = currentPlaying
   ? ` ${currentPlaying.user?.name || "Unknown Artist"} - ${currentPlaying.title}`
   : "";
 
-  const musicAlbumArt = currentPlaying?.artwork?.["480x480"]; 
-  
-  // check if text should slide
-  useEffect(() => {
-    if (textRef.current && containerRef.current) {
-      setShouldSlide(textRef.current.scrollWidth > containerRef.current.offsetWidth);
-    }
-  }, [titleArtist]);
+const title = currentPlaying.title;
+const artistName = currentPlaying.user.name;
 
+
+// check if text should slide
+useEffect(() => {
+  if (!textRef.current || !containerRef.current) return;
+
+  const textWidth = textRef.current.scrollWidth;
+  const containerWidth = containerRef.current.offsetWidth;
+
+  const overflow = textWidth - containerWidth;
+
+  if (overflow > 0) {
+    setShouldSlide(true);
+    const pixelsPerSecond = 50; 
+    const duration = textWidth / pixelsPerSecond;
+
+    setDurationSlide(Math.max(duration, 12));
+  } else {
+    setShouldSlide(false);
+  }
+}, [titleArtist]);
 
 
   return (
@@ -77,29 +109,36 @@ const titleArtist = currentPlaying
         <div className='flex flex-row w-full justify-center h-fit  gap-3 text-center p-2 '>
          
             <div className="rounded border-sky-100 border-[1.4px] w-12 h-12 overflow-hidden flex items-center justify-center">
-  {loading ? (
-    <div className="w-3 h-3 border-2 border-gray-300 border-t-blue-400 rounded-full animate-spin"></div>
-              ) : (
-                <img className='rounded'
-                  src={coverPhotoSrc}
-                  alt="Album Cover"
-                  style={{ width: "100%", height: "100%", objectFit: "cover" }}
-                />
-              )}
-            </div>
-           <div ref={containerRef}
+       
+            {loading ? (
+              <div className="w-3 h-3 border-2 border-gray-300 border-t-blue-400 rounded-full animate-spin"></div>
+                        ) : (
+                          <img className='rounded'
+                            src={coverPhotoSrc}
+                            alt="Album Cover"
+                            style={{ width: "100%", height: "100%", objectFit: "cover" }}
+                          />
+                        )}
+                      </div>
+                <div ref={containerRef}
+                  className='flex flex-col w-32 overflow-hidden justify-center'>
+                    <p ref={textRef} 
+                    className="whitespace-nowrap text-sm font-bold "
+                    style={
+                      shouldSlide ?
+                      {
+                        animation: `slide ${durationSlide}s linear infinite`,
+                      }:{}
+                    }
+                        >  
+                        {title}
+                        </p>
+                    <p className='label-font '>Artist: {artistName}</p>
 
-           className='flex flex-col w-32 overflow-hidden justify-center'>
-            <p ref={textRef} 
-            className={`whitespace-nowrap text-sm font-bold 
-              ${shouldSlide ?
-                 "title-slide"
-                 : ""
-                } `}>{titleArtist}</p>
-            <p className='label-font font-bold '>Currently Playing</p>
-          </div>
-        </div>
+                  </div>
+                </div>
 
+    {/*button controls  */}
       <div className="flex flex-row justify-center h-fit pb-2  gap-5">
 
         {/* SHUFFLE */}
@@ -108,6 +147,8 @@ const titleArtist = currentPlaying
             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13.484 9.166 15 7h5m0 0-3-3m3 3-3 3M4 17h4l1.577-2.253M4 7h4l7 10h5m0 0-3 3m3-3-3-3"/>
           </svg>
        </button>
+
+
         {/* BACK */}
         <button >
           <svg className="w-5 h-5 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
@@ -117,7 +158,6 @@ const titleArtist = currentPlaying
 
 
         {/* PLAY */}
-
         <button className='bg-blue-950 rounded-full p-1'
          onClick={togglePlayPause}>
 
